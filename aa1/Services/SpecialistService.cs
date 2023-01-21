f18b0919e2d333933f7150d105c1004b51454933
Vilipi <vicentury_@hotmail.com>
2023-01-22 00:33:31 +0100

    SpecialistService add search functionality

--- aa1/Services/SpecialistService.cs
+++ aa1/Services/SpecialistService.cs
@@ -27,7 +27,7 @@ namespace aa1.Services
         {
             var specilasitsString = _jsonService.GetListFromFile("specialist");
             var specialists = _jsonService.DeserializeSpecialistsJsonFile(specilasitsString);
-            Console.WriteLine("Pelase login using your credentials\n");
+            Console.WriteLine("Please login using your credentials\n");
 
             Console.WriteLine("Username: ");
             var inputUsername = Console.ReadLine();
@@ -64,11 +64,12 @@ namespace aa1.Services
 
             Console.WriteLine("\nPlease write one of the following numbers:");
             Console.WriteLine(" - 1: List my appointments");
-            Console.WriteLine(" - 2: Modify my appointments");
-            Console.WriteLine(" - 3: Back");
+            Console.WriteLine(" - 2: Search appointment");
+            Console.WriteLine(" - 3: Modify my appointments");
+            Console.WriteLine(" - 4: Back");
             string loggedSpecialistAction = Console.ReadLine();
 
-            while (loggedSpecialistAction != "1" && loggedSpecialistAction != "2" && loggedSpecialistAction != "3")
+            while (loggedSpecialistAction != "1" && loggedSpecialistAction != "2" && loggedSpecialistAction != "3" && loggedSpecialistAction != "4")
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Wrong number, try again");
@@ -81,162 +82,210 @@ namespace aa1.Services
                 case "1":
                     listAppointments(appointments, specialist);
                     return 1;
-                    break;
                 case "2":
+                    searchAppointment(appointments, specialist);
+                    return 1;
+                case "3":
                     modifyAppointment(appointments, specialist);
                     return 1;
-                    break;
-                case "3":
-                    return 1;
-                    break;
                 default:
-                    return 1;
+                    return 0;
             }
         }
 
         private void listAppointments(List<Appointment> appointments, Specialist specialist)
         {
             var specialistAppointments = appointments.FindAll(e => e.Specialist.UserName == specialist.UserName).ToList();
-            int count = 1;
-            specialistAppointments.ForEach(e =>
+            if(specialistAppointments.Count < 1)
             {
-                Console.WriteLine($"\n· {count}:");
-                Console.WriteLine($" - Id: {e.Id}");
-                string status = e.IsCompleted == true ? "finished": "active";
-                Console.WriteLine($" - Status: {status}");
-                Console.WriteLine($" - Name: {e.Name}");
-                Console.WriteLine($" - Patient: {e.Patient.Name} {e.Patient.LastName}");
-                string price = e.Price == null ? "Pending" : $"{e.Price}";
-                Console.WriteLine($" - Price: {price}");
-                string comments = e.SpecialistComment == null ? "Pending" : $"{e.SpecialistComment}";
-                Console.WriteLine($" - Comments: {comments}");
-                count++;
-            });
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nYou do not have any appointments");
+                Console.ResetColor();
+            }
+            else
+            {
+                int count = 1;
+                specialistAppointments.ForEach(e =>
+                {
+                    Console.WriteLine($"\n· {count}:");
+                    Console.WriteLine($" - Id: {e.Id}");
+                    string status = e.IsCompleted == true ? "finished" : "active";
+                    Console.WriteLine($" - Status: {status}");
+                    Console.WriteLine($" - Name: {e.Name}");
+                    Console.WriteLine($" - Patient: {e.Patient.Name} {e.Patient.LastName}");
+                    string price = e.Price == null ? "Pending" : $"{e.Price}";
+                    Console.WriteLine($" - Price: {price}");
+                    string comments = e.SpecialistComment == null ? "Pending" : $"{e.SpecialistComment}";
+                    Console.WriteLine($" - Comments: {comments}");
+                    count++;
+                });
+            }
+            
         }
 
         private int modifyAppointment(List<Appointment> appointments, Specialist specialist)
         {
             var specialistAppointments = appointments.FindAll(e => e.Specialist.UserName == specialist.UserName && e.IsCompleted == false).ToList();
-
-            Console.WriteLine("Please write the ID number of the appointment you want to edit");
-            string appointmentInput = Console.ReadLine();
-            int appointmentToModifylInt;
-            bool appointmentToModifyBool = int.TryParse(appointmentInput, out appointmentToModifylInt);
-
-            appointmentToModifylInt = appointmentToModifyBool == true ? Int32.Parse(appointmentInput) : 0;
-
-            var appointmentSelected = specialistAppointments.Find(e => e.Id == appointmentToModifylInt);
-
-            while (appointmentSelected ==  null)
+            if(specialistAppointments.Count < 1)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Wrong number id, try again\n");
+                Console.WriteLine("\nYou do not have any appointments");
                 Console.ResetColor();
-                specialistAppointments.ForEach(e =>
-                {
-                    Console.WriteLine($" - Id: {e.Id}: {e.Name}");
-                });
-                appointmentInput = Console.ReadLine();
-                appointmentToModifyBool = int.TryParse(appointmentInput, out appointmentToModifylInt);
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("Please write the ID number of the appointment you want to edit");
+                string appointmentInput = Console.ReadLine();
+                int appointmentToModifylInt;
+                bool appointmentToModifyBool = int.TryParse(appointmentInput, out appointmentToModifylInt);
+
                 appointmentToModifylInt = appointmentToModifyBool == true ? Int32.Parse(appointmentInput) : 0;
-                appointmentSelected = specialistAppointments.Find(e => e.Id == appointmentToModifylInt);
-            }
 
-            Console.WriteLine("Appointment selected. Choose what you want to modify:");
-            Console.WriteLine(" - 1: Price appointment");
-            Console.WriteLine(" - 2: Comment appointment");
-            Console.WriteLine(" - 3: Complete appointment");
-            Console.WriteLine(" - 4: Back");
-            string  apointmentModification = Console.ReadLine();
+                var appointmentSelected = specialistAppointments.Find(e => e.Id == appointmentToModifylInt);
 
-            while (apointmentModification != "1" && apointmentModification != "2" && apointmentModification != "3" && apointmentModification != "4")
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Wrong number, try again\n");
-                Console.ResetColor();
-                apointmentModification = Console.ReadLine();
-            }
-
-            switch (apointmentModification)
-            {
-                case "1":
-                    if (appointmentSelected.Price != 0)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("This appointment already has a price\n");
-                        Console.ResetColor();
-                        return 0;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Add price:");
-                        string newPriceString = Console.ReadLine();
-                        decimal newPriceDec;
-                        var isPriceCorrect = decimal.TryParse(newPriceString, out newPriceDec);
-                        while (isPriceCorrect == false)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Wrong price, try again");
-                            Console.ResetColor();
-                            newPriceString = Console.ReadLine();
-                            isPriceCorrect = decimal.TryParse(newPriceString, out newPriceDec);
-                        }
-                        appointmentSelected.Price = decimal.Parse(newPriceString);
-
-                        int index1 = appointments.FindIndex(e => e.Id == appointmentSelected.Id);
-                        appointments[index1] = appointmentSelected;
-                        _jsonService.SerealizeAppointmentJsonFile(appointments); //modifiying appointment file
-
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("\nPrice changed\n");
-                        Console.ResetColor();
-                    }
-                    return 1;
-
-                case "2":
-                    if (appointmentSelected.SpecialistComment == null)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("This appointment already has a comment\n");
-                        Console.ResetColor();
-                        return 0;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Add comment:");
-                        string newCommentString = Console.ReadLine();
-                        while (newCommentString == null)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Wrong comment, try again");
-                            Console.ResetColor();
-                            newCommentString = Console.ReadLine();
-                        }
-                        appointmentSelected.SpecialistComment = newCommentString;
-
-                        int index2 = appointments.FindIndex(e => e.Id == appointmentSelected.Id);
-                        appointments[index2] = appointmentSelected;
-                        _jsonService.SerealizeAppointmentJsonFile(appointments); //modifiying appointment file
-
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("\nComment changed\n");
-                        Console.ResetColor();
-                    }
-                    return 1;
-
-                case "3":
-                    appointmentSelected.IsCompleted = true;
-                    int index = appointments.FindIndex(e => e.Id == appointmentSelected.Id);
-                    appointments[index] = appointmentSelected;
-                    _jsonService.SerealizeAppointmentJsonFile(appointments); //modifiying appointment file
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("\nAppointment competed!\n");
+                while (appointmentSelected == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong number id, try again\n");
                     Console.ResetColor();
-                    return 1;
-                default:
-                    return 1;
+                    specialistAppointments.ForEach(e =>
+                    {
+                        Console.WriteLine($" - Id: {e.Id}: {e.Name}");
+                    });
+                    appointmentInput = Console.ReadLine();
+                    appointmentToModifyBool = int.TryParse(appointmentInput, out appointmentToModifylInt);
+                    appointmentToModifylInt = appointmentToModifyBool == true ? Int32.Parse(appointmentInput) : 0;
+                    appointmentSelected = specialistAppointments.Find(e => e.Id == appointmentToModifylInt);
+                }
+
+                Console.WriteLine("Appointment selected. Choose what you want to modify:");
+                Console.WriteLine(" - 1: Price appointment");
+                Console.WriteLine(" - 2: Comment appointment");
+                Console.WriteLine(" - 3: Complete appointment");
+                Console.WriteLine(" - 4: Back");
+                string apointmentModification = Console.ReadLine();
+
+                while (apointmentModification != "1" && apointmentModification != "2" && apointmentModification != "3" && apointmentModification != "4")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong number, try again\n");
+                    Console.ResetColor();
+                    apointmentModification = Console.ReadLine();
+                }
+
+                switch (apointmentModification)
+                {
+                    case "1":
+                        if (appointmentSelected.Price != 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("This appointment already has a price\n");
+                            Console.ResetColor();
+                            return 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Add price:");
+                            string newPriceString = Console.ReadLine();
+                            decimal newPriceDec;
+                            var isPriceCorrect = decimal.TryParse(newPriceString, out newPriceDec);
+                            while (isPriceCorrect == false)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Wrong price, try again");
+                                Console.ResetColor();
+                                newPriceString = Console.ReadLine();
+                                isPriceCorrect = decimal.TryParse(newPriceString, out newPriceDec);
+                            }
+                            appointmentSelected.Price = decimal.Parse(newPriceString);
+
+                            int index1 = appointments.FindIndex(e => e.Id == appointmentSelected.Id);
+                            appointments[index1] = appointmentSelected;
+                            _jsonService.SerealizeAppointmentJsonFile(appointments); //modifiying appointment file
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nPrice changed\n");
+                            Console.ResetColor();
+                        }
+                        return 1;
+
+                    case "2":
+                        if (appointmentSelected.SpecialistComment == null)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("This appointment already has a comment\n");
+                            Console.ResetColor();
+                            return 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Add comment:");
+                            string newCommentString = Console.ReadLine();
+                            while (newCommentString == null)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Wrong comment, try again");
+                                Console.ResetColor();
+                                newCommentString = Console.ReadLine();
+                            }
+                            appointmentSelected.SpecialistComment = newCommentString;
+
+                            int index2 = appointments.FindIndex(e => e.Id == appointmentSelected.Id);
+                            appointments[index2] = appointmentSelected;
+                            _jsonService.SerealizeAppointmentJsonFile(appointments); //modifiying appointment file
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nComment changed\n");
+                            Console.ResetColor();
+                        }
+                        return 1;
+
+                    case "3":
+                        appointmentSelected.IsCompleted = true;
+                        int index = appointments.FindIndex(e => e.Id == appointmentSelected.Id);
+                        appointments[index] = appointmentSelected;
+                        _jsonService.SerealizeAppointmentJsonFile(appointments); //modifiying appointment file
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\nAppointment competed!\n");
+                        Console.ResetColor();
+                        return 1;
+                    default:
+                        return 1;
+                }
             }
+            
+        }
+
+        private int searchAppointment(List<Appointment> appointments, Specialist specialist)
+        {
+            var specialistAppointments = appointments.FindAll(e => e.Specialist.UserName == specialist.UserName).ToList();
+            Console.WriteLine("Introduce the name of the patient to find their appointments");
+            string patientNameInput = Console.ReadLine();
+
+            var patientAppointments = new List<Appointment>();
+            specialistAppointments.ForEach(e =>
+            {
+                if (e.Patient.Name.Contains(patientNameInput))
+                {
+                    patientAppointments.Add(e);
+                }
+            });
+            if(patientAppointments.Count < 1)
+            {
+                Console.WriteLine("Appointmets for this patients not found");
+            }
+            else
+            {
+                patientAppointments.ForEach(e =>
+                {
+                    Console.WriteLine($" - Id: #{e.Id}");
+                    Console.WriteLine($" - Name: {e.Name}");
+                    Console.WriteLine($" - Completed: #{e.IsCompleted}\n");
+                });
+            }
+            return 1;
         }
     }
 }
